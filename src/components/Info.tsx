a92f7e08e25226b9158ede66ac13d226cdc39d53
Syakhisk Al-Azmi <mail.syakhisk@gmail.com>
2023-03-30 09:11:42 +0700

    feat: add exit button from session, cancel username modal

--- src/components/Info.tsx
+++ src/components/Info.tsx
@@ -1,14 +1,22 @@
-import { UserIcon } from "@heroicons/react/24/solid";
+import { ChevronLeftIcon, UserIcon } from "@heroicons/react/24/solid";
 import useSessionStore, { logout } from "@/stores/session-store";
+import { Link } from "react-router-dom";
 
 const Info = () => {
   const username = useSessionStore((s) => s.username);
   const session = useSessionStore((s) => s.session);
   return (
     <div className="flex border justify-between px-4 py-1">
-      <div className="flex md:flex-col max-md:items-center max-md:gap-2">
-        <div className="text-xs leading-none">Session Code</div>
-        <div className="font-mono max-md:leading-none">#{session.id}</div>
+      <div className="flex items-center gap-2">
+        <Link to="/">
+          <button className="-ml-2 outline outline-1 outline-muted p-2 rounded hover:outline-primary group transition-colors duration-150">
+            <ChevronLeftIcon className="h-4 w-4 group-hover:text-primary transition-colors duration-150" />
+          </button>
+        </Link>
+        <div className="flex md:flex-col max-md:items-center max-md:gap-2">
+          <div className="text-xs leading-none">Session Code</div>
+          <div className="font-mono max-md:leading-none">#{session.id}</div>
+        </div>
       </div>
 
       <button
