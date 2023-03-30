7fc8a0abfa907b753240500d8d88fdbc96260cb8
Syakhisk Al-Azmi <mail.syakhisk@gmail.com>
2023-03-30 08:45:12 +0700

    feat: add mkcert, https for localhost, youtube iframe things

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,12 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import eslint from "vite-plugin-eslint";
 import * as path from "path";
+import mkcert from "vite-plugin-mkcert";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), eslint({ useEslintrc: true })],
+  server: { https: true },
+  plugins: [react(), eslint({ useEslintrc: true }), mkcert()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
